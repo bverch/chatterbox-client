cb5c579f1b799012e39bebf9331951bba68e2e63
Hack Reactor Student <student@hackreactor.com>
2017-10-21 14:46:20 -0700

    Add friends & filtering by friends, refactor for XSS prevention, fix getMessages to retrieve from the last day of messages, fails some tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,12 +3,12 @@
 var app = {
 
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  friends: {},
 
   init: function() {
     $(document).ready(function() {
       $('#postMessage').on('click', function(event) {
-        var message = app.composeMessage();
-        app.postMessage(message);
+        app.handleSubmit();
       });
 
       $('#getMessages').on('click', function(event) {
@@ -22,10 +22,12 @@ var app = {
       $('#roomSelector').on('click', function(event) {
         app.getRoomMessages();
       }); 
-      // app.renderRoom('lobby');
+ 
       app.getMessages();
 
     });
+
+  
 
   // calls render messages on initial fetch of messages
   // appendTo("body")
@@ -44,11 +46,12 @@ var app = {
   },
 
   postMessage: function(message) {
+    
     $.ajax({
       url: app.server,
       type: 'POST',
       data: message, // ?? We removed JSONStringify here from the message arg
-      //contentType: 'application/json; charset=utf-8',
+      // contentType: 'application/json; charset=utf-8',
       success: function (data) {
         console.log('chatterbox: Message sent');
       },
@@ -59,17 +62,24 @@ var app = {
     });
   },
 
-  getMessages: function() {
+  handleSubmit: function() {
+    var message = app.composeMessage();
+    app.postMessage(message);
+  },
+
+  getMessages: function(messageFilter) {
+
     $.ajax({
       url: app.server,
       type: 'GET',
       'Retry-After': 6000,
+      data: 'order=-createdAt',
       success: function(data) {
-
-// Must render these when they come back.
+        // Must render these when they come back.
         app.clearMessages();
         app.fetchRoomNames(data);
-        for (var i = data['results'].length - 1; i > 0; i--) {
+        // app.renderFriendList();
+        for (var i = data['results'].length - 1; i >= 1; i--) {
           app.renderMessage(data['results'][i]);
         }
       },
@@ -79,16 +89,50 @@ var app = {
     });
   },
 
+  escapeString: function(string) {
+  // loop over the string if the char is '<' substitute &length
+    for (var i = 0; i < string.length; i++) {
+      if (string[i] === '<') {
+        string = string.slice(0, i) + '&lt' + string.slice(i + 1);
+      } else if (string[i] === '>') {
+        string = string.slice(0, i) + '&gt' + string.slice(i + 1);
+      } else if (string[i] === '&') {
+        string = string.slice(0, i) + '&amp' + string.slice(i + 1);
+      } else if (string[i] === '"') {
+        string = string.slice(0, i) + '&quot' + string.slice(i + 1);
+      } else if (string[i] === "'") {
+        string = string.slice(0, i) + '&#x27' + string.slice(i + 1); 
+      } else if (string[i] === '/') {
+        string = string.slice(0, i) + '&#x2f' + string.slice(i + 1); 
+      }
+    }
+    return string;
+  },
+
+
 // render message onto the DOM
   renderMessage: function(messageObj) {
-    //TODO: maybe we should change the test instead of doing this hacky thing
+    // does this message content match the filters?
+    // try to see filter that is selected for both room and friends    
+
+
+    var span = $('<span/>');
+    span.click(function() { app.handleUsernameClick(span); });
+    span.text(messageObj['username']);
+
+    var div = $('<div/>');
+    div.text(' says: ' + messageObj['text'] + '  in room: ' + messageObj['roomname']);
+    div.prepend(span);
+
     if (document.getElementById('roomSelect').value === '') {
-      var string = messageObj['username'] + '\n' + messageObj['text'] + '\n' + messageObj['roomname'] + '\n' + '';
-      $('#chats').append('<div class="chat">' + string + '</div>');
+      $('#chats').append(div);
+      $('#chats').append('<br>');
     }
-    if (document.getElementById('roomSelect').value === messageObj['roomname']) {
-      var string = messageObj['username'] + '\n' + messageObj['text'] + '\n' + messageObj['roomname'] + '\n' + '';
-      $('#chats').append('<div class="chat">' + string + '</div>');
+    if (document.getElementById('roomSelect').value === escape(messageObj['roomname']) || 
+        document.getElementById('friendSelect').value === escape(messageObj['username'])
+      ) {
+      $('#chats').append(div);
+      $('#chats').append('<br>');
     }
   },
 
@@ -104,13 +148,48 @@ var app = {
     // we are adding it to the list of options
     var contains = false;
     for (var i = 0; i < children.length; i++) {
-      if (children[i].value === newRoomName) {
+      if (children[i].value === app.escapeString(newRoomName)) {
         contains = true;
       }
     }
 
+    var option = $('<option/>');
+    option.attr('value', newRoomName);
+    option.text(app.escapeString(newRoomName));
+
     if (!contains) {
-      $('#roomSelect').append('<option value="' + newRoomName + '">' + newRoomName + '</option>');
+      $('#roomSelect').append(option);
+    }
+  },
+
+  renderFriendList: function() {
+    // we are adding it to the list of options
+    document.getElementById('friendSelect').innerHTML = '';
+    for (var k in app.friends) {
+      var option = $('<option/>');
+      option.attr('value', k);
+      option.text(app.escapeString(k));
+      $('#friendSelect').append(option);
+    }
+  },
+
+  
+  renderFriendMessages: function(friendName) {
+    var children = $('#friendSelect').children(); 
+    // we are adding it to the list of options
+    var contains = false;
+    for (var i = 0; i < children.length; i++) {
+      if (children[i].value === app.escapeString(friendName)) {
+        contains = true;
+      }
+    }
+
+    var option = $('<option/>');
+    option.attr('value', friendName);
+    option.text(app.escapeString(friendName));
+
+    if (!contains) {
+      $('#friendSelect').append(option);
     }
   },
 
@@ -127,8 +206,25 @@ var app = {
     });
   },
 
-  handleUsernameClick: function() {
+  fetchFriendNames: function(resultsObj) { // should just be our friend list.
+    var friendNames = {};
+    // so we can iterate the array of message objects
+    for (var i = 0; i < resultsObj['results'].length; i++) {
+      // pull out the roomnames to an obj where key is room name and value is true
+      friendNames[resultsObj['results'][i]['username']] = 1;
+    }
+// Object.keys(on the roomObj) and loop over and renderRoom.
+    Object.keys(friendNames).forEach(function(item) {
+      app.renderFriendMessages(item);
+    });
+  },
 
+
+
+  handleUsernameClick: function(span) {
+    console.log(span[0].textContent);
+    app.friends[span[0].textContent] = span[0].textContent;
+    app.renderFriendList();
   }
 
 
